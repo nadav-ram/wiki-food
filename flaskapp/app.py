40141db7af4ab638a3f2ada6155a5ba2c1e9bd3d
Nadav Ram <super.nadav.ram@gmail.com>
2019-06-01 20:10:18 +0300

    home page

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
-from data import data
+from data import data as data
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+
+    foods = []
+    for view in data:
+        if view['food'] in foods:
+            continue
+        foods.append(view['food'])
+
+    return render_template('layout.html', foods=foods)
 
 
 if __name__ == '__main__':
