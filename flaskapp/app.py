251fb6781d6f263ad1b3a78d41f5a3c82d5e6e72
Nadav Ram <super.nadav.ram@gmail.com>
2019-06-01 21:43:23 +0300

    Added food page and locations to data

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -2,18 +2,31 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from data import data as data
 
 app = Flask(__name__)
+app.secret_key = '12345678'
 
 
+# Get foods without duplicates
+foods = []
+for view in data:
+    if view['food'] in foods:
+        continue
+    foods.append(view['food'])
+
+
+@app.route('/home')
 @app.route('/')
 def home():
+    return render_template('home.html', foods=foods)
 
-    foods = []
-    for view in data:
-        if view['food'] in foods:
-            continue
-        foods.append(view['food'])
 
-    return render_template('layout.html', foods=foods)
+@app.route('/food/<food>')
+def food(food):
+
+    if food not in foods:
+        flash(f'{food} not found', 'danger')
+        return redirect(url_for('home'))
+
+    return render_template('food.html', food=food)
 
 
 if __name__ == '__main__':
--- flaskapp/data.py
+++ flaskapp/data.py
@@ -3,70 +3,84 @@ data = [
         'food': 'pizza',
         'year': 2018,
         'month': 'jan',
+        'location': 'usa'
     },
     {
         'food': 'banana',
         'year': 2018,
         'month': 'feb',
+        'location': 'israel'
     },
     {
         'food': 'chocolate',
         'year': 2019,
         'month': 'mar',
+        'location': 'germany'
     },
     {
         'food': 'banana',
         'year': 2018,
         'month': 'apr',
+        'location': 'usa'
     },
     {
         'food': 'pizza',
         'year': 2019,
         'month': 'may',
+        'location': 'usa'
     },
     {
         'food': 'stake',
         'year': 2019,
         'month': 'jun',
+        'location': 'japan'
     },
     {
         'food': 'pizza',
         'year': 2018,
         'month': 'jul',
+        'location': 'canada'
     },
     {
         'food': 'stake',
         'year': 2018,
         'month': 'aug',
+        'location': 'canada'
     },
     {
         'food': 'pizza',
         'year': 2018,
         'month': 'sep',
+        'location': 'bulgaria'
     },
     {
         'food': 'stake',
         'year': 2019,
         'month': 'oct',
+        'location': 'israel'
     },
     {
         'food': 'pizza',
         'year': 2018,
         'month': 'nov',
+        'location': 'usa'
     },
     {
         'food': 'chocolate',
         'year': 2018,
         'month': 'dec',
+        'location': 'israel'
     },
     {
         'food': 'sushi',
         'year': 2019,
         'month': 'jan',
+        'location': 'israel'
     },
     {
         'food': 'banana',
         'year': 2019,
         'month': 'feb',
+        'location': 'bulgaria'
     },
 ]
