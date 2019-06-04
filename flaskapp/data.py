4af293f2bdae993a336595353f53beddefe47967
Nadav Ram <super.nadav.ram@gmail.com>
2019-06-04 15:09:38 +0300

    Added more data points

--- flaskapp/data.py
+++ flaskapp/data.py
@@ -83,4 +83,88 @@ data = [
         'month': '2',
         'location': [42.697708, 23.321867]
     },
+    {
+        'food': 'salad',
+        'year': 2018,
+        'month': '1',
+        'location': [39.91666667, 116.383333]
+    },
+    {
+        'food': 'banana',
+        'year': 2019,
+        'month': '2',
+        'location': [60.16666667, 24.933333]
+    },
+    {
+        'food': 'salad',
+        'year': 2018,
+        'month': '3',
+        'location': [19.43333333, -99.133333]
+    },
+    {
+        'food': 'salad',
+        'year': 2019,
+        'month': '4',
+        'location': [34.01666667, -6.816667]
+    },
+    {
+        'food': 'pizza',
+        'year': 2018,
+        'month': '5',
+        'location': [34.01666667, -6.816667]
+    },
+    {
+        'food': 'steak',
+        'year': 2019,
+        'month': '6',
+        'location': [-6.8, 39.283333]
+    },
+    {
+        'food': 'pizza',
+        'year': 2018,
+        'month': '7',
+        'location': [56.130367, -106.346771]
+    },
+    {
+        'food': 'steak',
+        'year': 2019,
+        'month': '8',
+        'location': [56.130367, -106.346771]
+    },
+    {
+        'food': 'pizza',
+        'year': 2018,
+        'month': '9',
+        'location': [42.697708, 23.321867]
+    },
+    {
+        'food': 'steak',
+        'year': 2019,
+        'month': '10',
+        'location': [60.16666667, 24.933333]
+    },
+    {
+        'food': 'pizza',
+        'year': 2018,
+        'month': '11',
+        'location': [39.91666667, 116.383333]
+    },
+    {
+        'food': 'salad',
+        'year': 2019,
+        'month': '12',
+        'location': [60.16666667, 24.933333]
+    },
+    {
+        'food': 'sushi',
+        'year': 2018,
+        'month': '1',
+        'location': [60.16666667, 24.933333]
+    },
+    {
+        'food': 'salad',
+        'year': 2019,
+        'month': '2',
+        'location': [42.697708, 23.321867]
+    }
 ]
