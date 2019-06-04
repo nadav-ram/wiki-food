f478ec8f8eef23e0bc5a97504d6b9f2c72b63a81
Nadav Ram <super.nadav.ram@gmail.com>
2019-06-04 19:09:09 +0300

    months are now ints

--- flaskapp/data.py
+++ flaskapp/data.py
@@ -2,169 +2,169 @@ data = [
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '1',
+        'month': 1,
         'location': [37.090240, -95.712891]
     },
     {
         'food': 'banana',
         'year': 2018,
-        'month': '2',
+        'month': 2,
         'location': [32.085300, 34.781769]
     },
     {
         'food': 'chocolate',
         'year': 2019,
-        'month': '3',
+        'month': 3,
         'location': [51.165691, 10.451526]
     },
     {
         'food': 'banana',
         'year': 2018,
-        'month': '4',
+        'month': 4,
         'location': [37.090240, -95.712891]
     },
     {
         'food': 'pizza',
         'year': 2019,
-        'month': '5',
+        'month': 5,
         'location': [37.090240, -95.712891]
     },
     {
         'food': 'steak',
         'year': 2019,
-        'month': '6',
+        'month': 6,
         'location': [35.689487, 139.691711]
     },
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '7',
+        'month': 7,
         'location': [56.130367, -106.346771]
     },
     {
         'food': 'steak',
         'year': 2018,
-        'month': '8',
+        'month': 8,
         'location': [56.130367, -106.346771]
     },
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '9',
+        'month': 9,
         'location': [42.697708, 23.321867]
     },
     {
         'food': 'steak',
         'year': 2019,
-        'month': '10',
+        'month': 10,
         'location': [32.085300, 34.781769]
     },
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '11',
+        'month': 11,
         'location': [37.090240, -95.712891]
     },
     {
         'food': 'chocolate',
         'year': 2018,
-        'month': '12',
+        'month': 12,
         'location': [32.085300, 34.781769]
     },
     {
         'food': 'sushi',
         'year': 2019,
-        'month': '1',
+        'month': 1,
         'location': [32.085300, 34.781769]
     },
     {
         'food': 'banana',
         'year': 2019,
-        'month': '2',
+        'month': 2,
         'location': [42.697708, 23.321867]
     },
     {
         'food': 'salad',
         'year': 2018,
-        'month': '1',
+        'month': 1,
         'location': [39.91666667, 116.383333]
     },
     {
         'food': 'banana',
         'year': 2019,
-        'month': '2',
+        'month': 2,
         'location': [60.16666667, 24.933333]
     },
     {
         'food': 'salad',
         'year': 2018,
-        'month': '3',
+        'month': 3,
         'location': [19.43333333, -99.133333]
     },
     {
         'food': 'salad',
         'year': 2019,
-        'month': '4',
+        'month': 4,
         'location': [34.01666667, -6.816667]
     },
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '5',
+        'month': 5,
         'location': [34.01666667, -6.816667]
     },
     {
         'food': 'steak',
         'year': 2019,
-        'month': '6',
+        'month': 6,
         'location': [-6.8, 39.283333]
     },
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '7',
+        'month': 7,
         'location': [56.130367, -106.346771]
     },
     {
         'food': 'steak',
         'year': 2019,
-        'month': '8',
+        'month': 8,
         'location': [56.130367, -106.346771]
     },
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '9',
+        'month': 9,
         'location': [42.697708, 23.321867]
     },
     {
         'food': 'steak',
         'year': 2019,
-        'month': '10',
+        'month': 10,
         'location': [60.16666667, 24.933333]
     },
     {
         'food': 'pizza',
         'year': 2018,
-        'month': '11',
+        'month': 11,
         'location': [39.91666667, 116.383333]
     },
     {
         'food': 'salad',
         'year': 2019,
-        'month': '12',
+        'month': 12,
         'location': [60.16666667, 24.933333]
     },
     {
         'food': 'sushi',
         'year': 2018,
-        'month': '1',
+        'month': 1,
         'location': [60.16666667, 24.933333]
     },
     {
         'food': 'salad',
         'year': 2019,
-        'month': '2',
+        'month': 2,
         'location': [42.697708, 23.321867]
     }
 ]
