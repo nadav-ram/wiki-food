5cd62d7c43ea3d47752a0d3b5f40590e2fe1f2d8
Nadav Ram <super.nadav.ram@gmail.com>
2019-06-04 14:44:26 +0300

    Improved map rendering

--- flaskapp/render_map.py
+++ flaskapp/render_map.py
@@ -11,14 +11,14 @@ def render_map(food):
             lats.append(view['location'][0])
             longs.append(view['location'][1])
 
-    gmap = gmplot.GoogleMapPlotter(25, 0, 2)
-    gmap.scatter(lats, longs, 'red', size=150000, marker=False)
+    gmap = gmplot.GoogleMapPlotter(25, 0, 2)  # lat, long, zoom(default)
+    gmap.scatter(lats, longs, 'red', size=150000, marker=False, alpha=1.0)
 
     gmap.draw('gmplot.html')
 
-    # Replace "\" with "/" in the html file
-    s = open('gmplot.html').read()
-    s = s.replace("\\", "/")
+    z = open('gmplot.html').read()
+    # THIS IS THE UPDATED ZOOM!! (CURRENTLY 1.8)
+    z = z.replace("zoom: 2", "zoom: 1.8")
     f = open('gmplot.html', 'w')
-    f.write(s)
+    f.write(z)
     f.close()
